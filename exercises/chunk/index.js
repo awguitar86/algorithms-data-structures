6a15b99d4dd6540f7bdbc4dd21f37d3f9d81ed90
awguitar86 <awguitar86@gmail.com>
2019-11-22 16:05:24 -0700

    completed the chunk and anagram exercises

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while(index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;
+
+/* 1st SOLUTION */
+// function chunk(array, size) {
+//     const chunked = [];                                  // Create empty array to hold chunks called 'chunked'
+    
+//     for (let el of array) {                              // For each element in the "unchunked" array
+//         const last = chunked[chunked.length - 1];        //      Retrieve the last element in 'chunked'
+//         if (!last || last.length === size) {             //      If last element does not exist, or if its length is equal to chunk size
+//             chunked.push([el]);                          //              Push a new chunk into 'chunked' with the current element
+//         } else {                                         //      Else add the current element into the chunk
+//             last.push(el);
+//         }
+//     }
+
+//     return chunked;
+// }
+
+/* 2nd SOLUTION */
+// function chunk(array, size) {
+//     const chunked = [];                                  // Create empty "chunked" array
+//     let index = 0;                                       // Create "index" start at 0
+
+//     while(index < array.length) {                        // While "index" is less than array.length
+//         chunked.push(array.slice(index, index + size));  //      Push a lice of length "size" from "array" into "chunked"
+//         index += size;                                   //      Add "size" to "index"
+//     }
+
+//     return chunked;
+// }
