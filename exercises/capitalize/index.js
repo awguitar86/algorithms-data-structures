a16e1e467518a04bdcf62a7f1854d22b1b874f33
awguitar86 <awguitar86@gmail.com>
2019-11-25 15:50:29 -0700

    Completed Sentance Capitalization and Printing Steps exercises.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,43 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for(let i = 1; i < str.length; i++) {
+        if(str[i -1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
 
 module.exports = capitalize;
+
+
+
+/* 1st SOLUTION */
+// function capitalize(str) {
+//     const words = [];                                            // Make and empty array called 'words'
+//     for (let word of str.split(' ')) {                           // Split the input string by spaces to get an array and For each word in the array
+//         words.push(word[0].toUpperCase() + word.slice(1));       //      Uppercase the first letter of the word. Join first letter with the rest of the string. Push result into 'words' array
+//     }
+//     return words.join(' ');                                      // Join 'words into a string and return it.
+// }
+
+
+/* 2nd SOLUTION */
+// function capitalize(str) {
+//     let result = str[0].toUpperCase();               // Create 'result' which is the first character of the input string capitalized.
+
+//     for(let i = 1; i < str.length; i++) {            // For each character in the string
+//         if(str[i -1] === ' ') {                      // IF the character to the left is a space
+//             result += str[i].toUpperCase();          // Capitalize it and add it to teh 'result' string
+//         } else {
+//             result += str[i];                        // Else add it to the 'result' string
+//         }
+//     }
+//     return result;
+// }
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,64 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    if(n === row) {
+        return;
+    }
+
+    if(n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    const add = stair.length <= row ? '#' : ' ';
+
+    steps(n, row, stair + add);
+}
 
 module.exports = steps;
+
+
+/* 1st SOLUTION */
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let column = 0; column < n; column++) {
+//             if(column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
+
+/* 2nd SOLUTION: Recursive Solution*/
+// function steps(n, row = 0, stair = '') {
+//     if(n === row) {
+//         return;
+//     }
+
+//     if(n === stair.length) {
+//         console.log(stair);
+//         return steps(n, row + 1);
+//     }
+
+//     const add = stair.length <= row ? '#' : ' ';
+
+//     steps(n, row, stair + add);
+// }
+
+
+/** RECURSION TIPS **/
+
+// Figure out the bare minimum pieces of inormation to represent your problem
+
+// Give reasonable defaults to the bare minimum pieces of info
+
+// Check the base case. Is there any work left to do? If not, return.
+
+// Do some work. Call your function again, making sure the arguments have changed in some fashion.
